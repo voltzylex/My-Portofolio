e5fb857f3b4d325a21240daf3d8c6b51e83f58b3
volteeee <baymax732001@gmail.com>
2022-04-19 14:27:53 +0530

    Final Day completed

--- lib/main.dart
+++ lib/main.dart
@@ -13,165 +13,201 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(
-          top: 100.0,
-          left: 20.0,
-        ),
-        child: Column(
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                const CircleAvatar(
-                  backgroundImage: AssetImage("assets/images/sushil.jpg"),
-                  radius: 60,
-                ),
-                const SizedBox(
-                  width: 20,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: const [
-                    Text(
-                      "Sushil Kumar",
-                      style: TextStyle(
-                          fontSize: 35.0,
-                          fontWeight: FontWeight.w900,
-                          fontFamily: "fonts/Roboto-BoldItalic.tff"),
-                    ),
-                    //sized box used as a padding alternative
-                    SizedBox(
-                      height: 4,
-                    ),
-                    Text(
-                      "Designation",
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 60),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Row(
-                      children: const [
-                        Icon(
-                          Icons.school,
-                          size: 40,
-                        ),
-                        SizedBox(
-                          width: 15,
-                        ),
-                        Text(
-                          "IMT Faridabad",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ],
-                    ),
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage("assets/images/background.jpg"),
+          fit: BoxFit.cover,
+        )),
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 100.0,
+            left: 20.0,
+          ),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  const CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/sushil.jpg"),
+                    radius: 60,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Row(
-                      children: const [
-                        Icon(
-                          Icons.computer_rounded,
-                          size: 40,
-                        ),
-                        SizedBox(
-                          width: 15,
-                        ),
-                        Text(
-                          "School Name",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ],
-                    ),
+                  const SizedBox(
+                    width: 20,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Row(
-                      children: const [
-                        Icon(
-                          Icons.location_on,
-                          size: 40,
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text(
+                        "Sushil Kumar",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30.0,
+                            fontWeight: FontWeight.w900,
+                            fontFamily: "Variable"),
+                      ),
+                      //sized box used as a padding alternative
+                      SizedBox(
+                        height: 4,
+                      ),
+                      Text(
+                        "Designation",
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white,
                         ),
-                        SizedBox(
-                          width: 15,
-                        ),
-                        Text(
-                          "School Name",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Row(
-                      children: const [
-                        Icon(
-                          Icons.mail,
-                          size: 40,
-                        ),
-                        SizedBox(
-                          width: 15,
-                        ),
-                        Text(
-                          "School Name",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20),
-                    child: Row(
-                      children: const [
-                        Icon(
-                          Icons.phone,
-                          size: 40,
-                        ),
-                        SizedBox(
-                          width: 15,
-                        ),
-                        Text(
-                          "School Name",
-                          style: TextStyle(fontSize: 20),
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
                 ],
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Padding(
-              padding: EdgeInsets.all(15.0),
-              child: Text(
-                "My name is sushil , I had completed my 10 in 2018,and 12 in 2020, Now i m Doing BCA from MDU(Maharashi Dayanand University)rohtak, Haryana",
-                style: TextStyle(
-                    fontSize: 18,
-                    color: Color.fromARGB(255, 0, 0, 0),
-                    fontWeight: FontWeight.normal),
+              const SizedBox(
+                height: 30,
               ),
-            ),
-            const SizedBox(
-              height: 60,
-            ),
-            const Text(
-              "Made by Volteee",
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w900),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(left: 40),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Row(
+                        children: const [
+                          Icon(
+                            Icons.school,
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            "BCA",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Row(
+                        children: const [
+                          Icon(
+                            Icons.computer_rounded,
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            "Portofolio App",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Row(
+                        children: const [
+                          Icon(
+                            Icons.location_on,
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            "Faridabad,Haryana",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Row(
+                        children: const [
+                          Icon(
+                            Icons.mail,
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            "baymax732001@gmail.com",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: Row(
+                        children: const [
+                          Icon(
+                            Icons.phone,
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                          SizedBox(
+                            width: 15,
+                          ),
+                          Text(
+                            "7011822369",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Text(
+                  "My name is sushil , I had completed my 10 in 2018,and 12 in 2020, Now i m Doing BCA from MDU(Maharashi Dayanand University)rohtak, Haryana",
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Color.fromARGB(255, 245, 243, 243),
+                      // fontWeight: FontWeight.normal,
+                      fontFamily: "Roboto"),
+                ),
+              ),
+              const SizedBox(
+                height: 60,
+              ),
+              const Text(
+                "Made by Volteee",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w900,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
